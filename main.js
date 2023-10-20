a546eb396f542b03c358052cb17ae75eb572f80b
Kamila <105371103+kamilalmeidaa@users.noreply.github.com>
2023-10-20 11:13:58 -0300

    as

--- main.js
+++ main.js
@@ -2,4 +2,5 @@
 function tocaSomPom(){
 document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).play();
 }
-//chamada ou invocação da função
+//chamada ou invocação da função tocaSomPom ao clicar no botão
+document.querySelector(`#sounds/acessa-ai-estourado-mt-estourado.mp3`).onclick = tocaSomPom;
