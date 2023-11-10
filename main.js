bad49c38f3849a387079dbd595ca37ca6879cbd4
Kamila <105371103+kamilalmeidaa@users.noreply.github.com>
2023-11-10 11:27:30 -0300

    rytr

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 //Declaração/criação da função tocaSomPom
 function tocaSom(idElementoAudio){
-document.querySelector(idElementoAudio).play();
+    document.querySelector(idElementoAudio).play();
 }
 //criando a referência constante ListadeTeclas e buscando a classe.
 const ListadeTeclas = document.querySelectorAll ('.tecla');
-let contador = 0;
-while(contador < ListadeTeclas.length) {
-    const efeito = ListadeTeclas[contador].classList[1];
-    const idAudio = "#som_" + efeito;
-    ListadeTeclas[contador].onclick = function(){
+//criação de repetição
+
+for(let contador = 0;contador < ListadeTeclas.length;contador++) {
+    const teclas = ListadeTeclas[contador];
+    const efeito = teclas.classList[1];
+    const idAudio = `#som_${efeito}`;
+    teclas.onclick = function(){
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    console.log(contador);
+
 }
